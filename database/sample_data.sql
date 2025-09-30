2ecce23ef61ea320b2d8a82e06c8f11d627574db
Hedye Azizi <hedyeazizi0752@gmail.com>
2025-09-30 23:38:47 +0330

    feat(db): add sample users

---
+++ database/sample_data.sql
@@ -0,0 +1,6 @@
+
+-- Users
+INSERT INTO users (name, email, password, address, phone_number) VALUES
+('Ali Reza', 'ali@test.com', '1234', 'Tehran, Iran', '09120000001'),
+('Sara Ahmadi', 'sara@test.com', 'abcd', 'Isfahan, Iran', '09120000002'),
+('Mohammad Hosseini', 'mohammad@test.com', 'pass123', 'Shiraz, Iran', '09120000003');
